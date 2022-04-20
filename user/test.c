6631b0d84e0b917b73eab385ed93059e1a91a278
talhazi <talhazi114@gmail.com>
2022-04-20 07:50:15 +0300

    update according to syscalls tests

--- user/test.c
+++ user/test.c
@@ -2,16 +2,34 @@
 #include "user/user.h"
 #include "kernel/fcntl.h"
 
+void pause_system_dem(int interval, int pause_seconds, int loop_size) {
+    int pid = getpid();
+    for (int i = 0; i < loop_size; i++) {
+        if (i % interval == 0 && pid == getpid()) {
+            printf("pause system %d/%d completed.\n", i, loop_size);
+        }
+        if (i == loop_size / 2) {
+            pause_system(pause_seconds);
+        }
+    }
+    printf("\n");
+}
+
+void kill_system_dem(int interval, int loop_size) {
+    int pid = getpid();
+    for (int i = 0; i < loop_size; i++) {
+        if (i % interval == 0 && pid == getpid()) {
+            printf("kill system %d/%d completed.\n", i, loop_size);
+        }
+        if (i == loop_size / 2) {
+            kill_system();
+        }
+    }
+    printf("\n");
+}
 
 int main(int argc, char** argv){
-    pause_system(2);
-    int pid = fork();
-    if(pid!=0){ //parent
-        int status;
-        wait(&status);
-        fprintf(2,"Child %d finished with exit status %d\n", pid, status);
-    } else{ //child
-        fprintf(2, "Child is running\n");
-    }
+    pause_system_dem(100, 10, 1000);
+    kill_system_dem(100, 1000);
     exit(0);
 }
