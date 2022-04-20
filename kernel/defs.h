9de1f9c7c0606891a89810ff79f033697297a145
talhazi <talhazi114@gmail.com>
2022-04-20 07:47:45 +0300

    add signatures of print_stats

--- kernel/defs.h
+++ kernel/defs.h
@@ -104,6 +104,9 @@ void            yield(void);
 int             either_copyout(int user_dst, uint64 dst, void *src, uint64 len);
 int             either_copyin(void *dst, int user_src, uint64 src, uint64 len);
 void            procdump(void);
+int             pause_system(int);
+int             kill_system(void);
+int             print_stats(void);
 
 // swtch.S
 void            swtch(struct context*, struct context*);
